a958cd4c7f61e91f51534742fcb6a04b6b922ab1
Stacey Zheng <staceyzheng10@gmail.com>
2022-07-08 03:13:33 -0400

    rock tie, win, and lose working

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -8,3 +8,17 @@ computerChoice = options[randint(0, 2)]
 
 print("\nRock, paper, scissors, says shoot!\n")
 
+if (playerChoice == computerChoice) : 
+    print("It's a tie! You both picked " + playerChoice + ".")
+elif (playerChoice == "rock") :
+    if (computerChoice == "paper") :
+        print("You lose! Paper covers rock. You chose " + playerChoice \
+            + " but the computer chose " + computerChoice + ".")
+    elif (computerChoice == "scissors") :
+        print("You win! Rock crushes scissors. You chose " + playerChoice \
+            + " and the computer chose " + computerChoice + ".")
+            
+# rock crushes scissors
+# scissors cuts paper
+# paper covers rock
+print("\n")
