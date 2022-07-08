d01ee7ace25d9029456824e06dfab226b123882d
Stacey Zheng <staceyzheng10@gmail.com>
2022-07-08 03:43:02 -0400

    readme updated and emojis added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RockPaperScissors
-A Rock Paper Scissors game.
+A Rock 🪨 Paper 📃 Scissors ✂️ game. 
+
+If your choice is the same as the computer's then it's a tie.
+Otherwise, rock crushes scissors, scissors cuts paper, and paper covers rock.
+Will you win? Play a game to find out.
--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -6,33 +6,28 @@ playerChoice = input("\nPick \"rock\", \"paper\", or \"scissors\": ")
 
 computerChoice = options[randint(0, 2)]
 
-print("\nRock, paper, scissors, says shoot!\n")
+print("\nRock 🪨, paper 📃, scissors ✂️, says shoot!\n")
 
 if (playerChoice == computerChoice) : 
-    print("It's a tie! You both picked " + playerChoice + ".")
+    print("It's a tie! You both picked " + playerChoice + ".\n")
 elif (playerChoice == "rock") :
     if (computerChoice == "paper") :
-        print("You lose! Paper covers rock. You chose " + playerChoice \
-            + " but the computer chose " + computerChoice + ".")
+        print("You lose! Paper 📃 covers rock 🪨. You chose " + playerChoice \
+            + " but the computer chose " + computerChoice + ".\n")
     elif (computerChoice == "scissors") :
-        print("You win! Rock crushes scissors. You chose " + playerChoice \
-            + " and the computer chose " + computerChoice + ".")
+        print("You win! Rock 🪨 crushes scissors ✂️. You chose " + playerChoice \
+            + " and the computer chose " + computerChoice + ".\n")
 elif (playerChoice == "paper") :
     if (computerChoice == "scissors") :
-        print("You lose! Scissors cuts paper. You chose " + playerChoice \
-            + " but the computer chose " + computerChoice + ".")
+        print("You lose! Scissors ✂️ cuts paper 📃. You chose " + playerChoice \
+            + " but the computer chose " + computerChoice + ".\n")
     elif (computerChoice == "rock") :
-        print("You win! Paper covers rock. You chose " + playerChoice \
-            + " and the computer chose " + computerChoice + ".")
+        print("You win! Paper 📃 covers rock 🪨. You chose " + playerChoice \
+            + " and the computer chose " + computerChoice + ".\n")
 elif (playerChoice == "scissors") :
     if (computerChoice == "rock") :
-        print("You lose! Rock crushes scissors. You chose " + playerChoice \
-            + " but the computer chose " + computerChoice + ".")
+        print("You lose! Rock 🪨 crushes scissors ✂️. You chose " + playerChoice \
+            + " but the computer chose " + computerChoice + ".\n")
     elif (computerChoice == "paper") :
-        print("You win! Scissors cuts paper. You chose " + playerChoice \
-            + " and the computer chose " + computerChoice + ".")
-
-# Rock crushes scissors
-# Scissors cuts paper
-# Paper covers rock
-print("\n")
+        print("You win! Scissors ✂️ cuts paper 📃. You chose " + playerChoice \
+            + " and the computer chose " + computerChoice + ".\n")
