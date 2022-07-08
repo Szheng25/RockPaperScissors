74931b85374ce717d9d051595e29f72e961040f7
Stacey Zheng <staceyzheng10@gmail.com>
2022-07-08 03:47:05 -0400

    debug statements taken out

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -9,25 +9,19 @@ computerChoice = options[randint(0, 2)]
 print("\nRock 🪨, paper 📃, scissors ✂️, says shoot!\n")
 
 if (playerChoice == computerChoice) : 
-    print("It's a tie! You both picked " + playerChoice + ".\n")
+    print("It's a tie!")
 elif (playerChoice == "rock") :
     if (computerChoice == "paper") :
-        print("You lose! Paper 📃 covers rock 🪨. You chose " + playerChoice \
-            + " but the computer chose " + computerChoice + ".\n")
+        print("You lose! Paper 📃 covers rock 🪨.")
     elif (computerChoice == "scissors") :
-        print("You win! Rock 🪨 crushes scissors ✂️. You chose " + playerChoice \
-            + " and the computer chose " + computerChoice + ".\n")
+        print("You win! Rock 🪨 crushes scissors ✂️.")
 elif (playerChoice == "paper") :
     if (computerChoice == "scissors") :
-        print("You lose! Scissors ✂️ cuts paper 📃. You chose " + playerChoice \
-            + " but the computer chose " + computerChoice + ".\n")
+        print("You lose! Scissors ✂️ cuts paper 📃.")
     elif (computerChoice == "rock") :
-        print("You win! Paper 📃 covers rock 🪨. You chose " + playerChoice \
-            + " and the computer chose " + computerChoice + ".\n")
+        print("You win! Paper 📃 covers rock 🪨.")
 elif (playerChoice == "scissors") :
     if (computerChoice == "rock") :
-        print("You lose! Rock 🪨 crushes scissors ✂️. You chose " + playerChoice \
-            + " but the computer chose " + computerChoice + ".\n")
+        print("You lose! Rock 🪨 crushes scissors ✂️.")
     elif (computerChoice == "paper") :
-        print("You win! Scissors ✂️ cuts paper 📃. You chose " + playerChoice \
-            + " and the computer chose " + computerChoice + ".\n")
+        print("You win! Scissors ✂️ cuts paper 📃.")
