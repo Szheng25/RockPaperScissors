f0b0c5dd78d782ba45acd4419ca93b2bd899ae0c
Stacey Zheng <staceyzheng10@gmail.com>
2022-07-08 04:02:32 -0400

    emojis taken out and non valid choice taken care of

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -3,25 +3,27 @@ from random import randint
 options = ["rock", "paper", "scissors"]
 
 playerChoice = input("\nPick \"rock\", \"paper\", or \"scissors\": ")
+while (playerChoice not in options) :
+    playerChoice = input("\nNot a valid choice! Please pick from \"rock\", \"paper\", or \"scissors\": ")
 
 computerChoice = options[randint(0, 2)]
 
-print("\nRock 🪨, paper 📃, scissors ✂️, says shoot!\n")
+print("\nRock, paper, scissors, says shoot!\n")
 
 if (playerChoice == computerChoice) : 
     print("It's a tie!")
 elif (playerChoice == "rock") :
     if (computerChoice == "paper") :
-        print("You lose! Paper 📃 covers rock 🪨.")
+        print("You lose! Paper covers rock.\n")
     else:
-        print("You win! Rock 🪨 crushes scissors ✂️.")
+        print("You win! Rock crushes scissors.\n")
 elif (playerChoice == "paper") :
     if (computerChoice == "scissors") :
-        print("You lose! Scissors ✂️ cuts paper 📃.")
+        print("You lose! Scissors cuts paper.\n")
     else:
-        print("You win! Paper 📃 covers rock 🪨.")
+        print("You win! Paper covers rock.\n")
 else:
     if (computerChoice == "rock") :
-        print("You lose! Rock 🪨 crushes scissors ✂️.")
+        print("You lose! Rock crushes scissors.\n")
     else:
-        print("You win! Scissors ✂️ cuts paper 📃.")
+        print("You win! Scissors cuts paper.\n")
